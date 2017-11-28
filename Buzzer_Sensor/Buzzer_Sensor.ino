376f986886a4ce8aa884a551363e554ea0cc24dc
MARINA AZEVEDO FERREIRA SOARES <38781912862@jpmorgan.sp>
2017-11-28 21:49:37 -0200

    remocao de funcoes duplicadas

--- Buzzer_Sensor/Buzzer_Sensor.ino
+++ Buzzer_Sensor/Buzzer_Sensor.ino
@@ -1,30 +1,14 @@
 #include <RestClient.h>
 #include <Ultrasonic.h>
-#include <Keypad.h>
 #include <UIPEthernet.h>
 
-bool typing = false;//salvar estado da senha
-String senhaDigitada;
-String senha = "4321";
+
 int buzzer = 3;
 long ultimoEstadoPorta;
 bool alarmeAtivado;
 
 Ultrasonic ultrasonic(7, 8);
 
-const byte ROWS = 4; //four rows
-const byte COLS = 3; //three columns
-char keys[ROWS][COLS] = {
-  {'1', '2', '3'},
-  {'4', '5', '6'},
-  {'7', '8', '9'},
-  {'*', '0', '#'}
-};
-
-byte rowPins[ROWS] = {5, 4, 3, A0}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = {8, 7, 6}; //connect to the column pinouts of the keypad
-
-Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
 // Alterar o último valor para o id do seu kit
 const byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xFF };
@@ -42,10 +26,9 @@ String response = "";
 void setup() {
   Serial.begin(9600);
 
-  pinMode(A0, INPUT);
   pinMode(buzzer, OUTPUT);
 
-  if(Ethernet.begin(mac)) {
+  if (Ethernet.begin(mac)) {
     Serial.println("Conectado via DHCP");
     Serial.print("IP recebido:"); Serial.println(Ethernet.localIP());
   }
@@ -81,7 +64,6 @@ void loop() {
     distanciaPorta = Serial.parseInt();
   }
 
-  char key = keypad.getKey();
   long now = millis();
 
   if ((now % 5000 == 0) && alarmeAtivado) {
@@ -89,33 +71,7 @@ void loop() {
     tone(buzzer, 3000);
   }
 
-  if (key && alarmeAtivado) {
 
-    if (typing && key != '#') {
-      senhaDigitada += key;
-      Serial.println(senhaDigitada);
-    }
-
-    if (key == '*') {
-      senhaDigitada = "";
-      typing = true;
-      Serial.println("Digitando senha...");
-    }
-    if (key == '#') {
-      typing = false;
-      Serial.println("Finalizado");
-      if (senhaDigitada == senha) {
-        alarmeAtivado = false;
-        noTone (buzzer);
-        Serial.println("Alarme desativado");
-      }
-      else {
-        Serial.println("Acesso negado");
-        tone (buzzer, 2000);
-      }
-    }
-    Serial.println(key);
-  }
 
   if (distanciaPorta < 10 && distanciaPorta > 0) {
     Serial.println(distanciaPorta);
--- twilio/twilio.ino
+++ twilio/twilio.ino
@@ -1,9 +1,42 @@
+#include <SPI.h>
 #include <UIPEthernet.h>
+#include <utility/logging.h>
+#include <PubSubClient.h>
 #include <RestClient.h>
+
+int msg;
 
 // Alterar o último valor para o id do seu kit
 const byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xF1, 0x77 };
+
+void callback(char *topic, byte *payload, unsigned int length);
+
 EthernetClient ethclient;
+
+PubSubClient client("m10.cloudmqtt.com", 11356, callback, ethClient);
+
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  char c = payload;
+  Serial.println(c);
+  Serial.println(topic);
+
+  if (strcmp(topic, "validaSenha") == 0) {
+    msg = atoi(c);
+    if (msg == 1) {
+      Serial.println("Validou senha");
+    }
+  }
+
+}
+boolean reconnect() // Recebe função que reconecta ao servidor MQTT
+{
+  Serial.println("reconectando...");
+  if (client.connect("arduinoClient", "usuario", "senha")) {
+    client.subscribe("vaga/22");
+  }
+  return client.connected();
+}
 
 RestClient client = RestClient("192.168.3.186", 3000, ethclient);
 const char* sid = "AC9848c713c4f8d834a1a99b5ade904f60";
@@ -17,7 +50,7 @@ String response = "";
 void setup() {
   Serial.begin(9600);
   // Connect via DHCP
-  if(Ethernet.begin(mac)) {
+  if (Ethernet.begin(mac)) {
     Serial.println("Conectado via DHCP");
     Serial.print("IP recebido:"); Serial.println(Ethernet.localIP());
   }
@@ -32,9 +65,9 @@ void setup() {
   parametros.concat(to);
 
   parametros.concat("&from=");
-  parametros.concat(from);
+  parametros.concat("15744061737");
 
-  parametros.concat("&body=Mensagem Legal");
+  parametros.concat("&body=TESTE");
 
   Serial.println(parametros);
 
@@ -46,6 +79,6 @@ void setup() {
   delay(1000);
 }
 
-void loop(){
+void loop() {
 
 }
